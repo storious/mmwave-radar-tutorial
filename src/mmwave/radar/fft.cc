5ac79ee90c6f8a31bd05de7ba07e186f97c01bc5
storious <2227563488@qq.com>
2026-07-25 16:14:30 +0800

    fix: module partation implement unit

---
+++ src/mmwave/radar/fft.cc
@@ -0,0 +1,15 @@
+module mmwave.radar;
+
+import :types;
+
+namespace mmwave::radar {
+
+void RadarFFT::run_1d(ConstFrameView frame) noexcept {
+  // FFTW / custom FFT implementation
+}
+
+void RadarFFT::run_2d(ConstFrameView frame) noexcept {
+  // Doppler FFT
+}
+
+} // namespace mmwave::radar
