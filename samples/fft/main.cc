36c008d4d09c0b033875259f1222fd65ffd5a050
storious <2227563488@qq.com>
2026-07-26 19:12:54 +0800

    feat: add FFTW samples

---
+++ samples/fft/main.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+
+extern "C" {
+#include "fftw3.h"
+}
+
+import mmwave.radar;
+
+using namespace mmwave::radar;
+
+int main() {
+  constexpr int N = 8;
+
+  auto *input =
+      static_cast<fftw_complex *>(fftw_malloc(sizeof(fftw_complex) * N));
+
+  auto *output =
+      static_cast<fftw_complex *>(fftw_malloc(sizeof(fftw_complex) * N));
+
+  if (!input || !output) {
+    std::cerr << "alloc failed\n";
+    return 1;
+  }
+
+  FFTWMemPool pool;
+
+  RadarFFT fft(&pool);
+
+  auto plan = fft.create_1d_plan(N, input, output);
+
+  if (!plan) {
+    std::cerr << "create fft plan failed\n";
+    return 1;
+  }
+
+  plan.execute();
+
+  fftw_free(input);
+  fftw_free(output);
+
+  std::cout << "FFT OK\n";
+}
--- src/mmwave/radar/fft.cc
+++ src/mmwave/radar/fft.cc
@@ -1,15 +1,28 @@
 module mmwave.radar;
 
 import :types;
+import :fft;
 
 namespace mmwave::radar {
 
-void RadarFFT::run_1d(ConstFrameView frame) noexcept {
-  // FFTW / custom FFT implementation
+FFTWPlan::FFTWPlan(fftw_plan plan) noexcept : plan_(plan) {}
+
+FFTWPlan RadarFFT::create_1d_plan(int n, fftw_complex *in, fftw_complex *out) {
+  auto plan = fftw_plan_dft_1d(n, in, out, FORWARD, ESTIMATE);
+
+  return FFTWPlan(plan);
 }
 
-void RadarFFT::run_2d(ConstFrameView frame) noexcept {
+FrameView RadarFFT::run_1d(ConstFrameView frame) noexcept {
+  // FFTW / custom FFT implementation
+  FrameView outview;
+  return outview;
+}
+
+FrameView RadarFFT::run_2d(ConstFrameView frame) noexcept {
   // Doppler FFT
+  FrameView outview;
+  return outview;
 }
 
 } // namespace mmwave::radar
